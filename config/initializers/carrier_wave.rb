b061f415b9eff4845bd5738e9c089c60aed7587c
Ren Kataoka <530lotus@gmail.com>
2019-02-18 03:54:17 +0000

    Setting IAM

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,29 +1,12 @@
-
-require 'carrierwave/storage/abstract'
-require 'carrierwave/storage/file'
-require 'carrierwave/storage/fog'
-
-CarrierWave.configure do |config|
-  if Rails.env.production?
-    config.fog_provider = 'fog/aws'
-    config.fog_directory  = 'S3_BUCKET'
+if Rails.env.production?
+  CarrierWave.configure do |config|
     config.fog_credentials = {
-      provider: 'AWS',
-      aws_access_key_id: ENV['S3_ACCESS_KEY'],
-      aws_secret_access_key: ENV['S3_SECRET_KEY'],
-      region: ENV['S3_REGION'],
-      path_style: true
+      # Amazon S3用の設定
+      :provider              => 'AWS',
+      :region                => ENV['S3_REGION'],  
+      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+      :aws_secret_access_key => ENV['S3_SECRET_KEY']
     }
-    config.fog_public = true
-    config.fog_attributes = {'Cache-Control' => 'public, max-age=86400'}
-
-    config.remove_previously_stored_files_after_update = false
-
-    config.fog_directory = ENV['S3_BUCKET']
-    config.asset_host = ENV['S3_URL']
-  else
-    config.enable_processing = false if Rails.env.test?
+    config.fog_directory     =  ENV['S3_BUCKET']
   end
 end
-
-CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
