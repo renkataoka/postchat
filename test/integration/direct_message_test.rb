cd476368b73e405be57f32c1b9522d3c1ea8bf7b
Ren Kataoka <530lotus@gmail.com>
2019-02-21 13:03:41 +0000

    invalidate DM

--- test/integration/direct_message_test.rb
+++ test/integration/direct_message_test.rb
@@ -1,14 +1,14 @@
-require 'test_helper'
-
-class DirectMessageTest < ActionDispatch::IntegrationTest
-  def setup
-    @user = users(:michael)
-    @other = users(:archer)
-  end
-
-  test "message box" do
-    log_in_as(@user)
-    get user_path(@other)
-    assert_select 'section.message_box'
-  end
-end
+# require 'test_helper'
+#
+# class DirectMessageTest < ActionDispatch::IntegrationTest
+#   def setup
+#     @user = users(:michael)
+#     @other = users(:archer)
+#   end
+#
+#   test "message box" do
+#     log_in_as(@user)
+#     get user_path(@other)
+#     assert_select 'section.message_box'
+#   end
+# end
